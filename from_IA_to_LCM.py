c5b6dc69c0f15f256d4b2cdbb6c36db937a785e2
Martin Robinson <martinjrobins@gmail.com>
2019-09-05 05:21:16 +0100

    add transformation to do_transformation

--- from_IA_to_LCM.py
+++ from_IA_to_LCM.py
@@ -1,23 +1,35 @@
 import sys
 import os
 import pandas as pd
+import numpy as np
+
 
 def do_transformation(list_of_tuples):
-    pass
+    A = np.array([
+        [2.21447816e+00,   2.66453526e-14,   9.01681455e+04],
+        [8.15405994e-15,   1.85592642e+00,   2.58158836e+04],
+        [0.00000000e+00,   0.00000000e+00,   1.00000000e+00],
+    ])
+
+    for i, point in enumerate(list_of_tuples):
+        list_of_tuples[i] = A.dot(point+(1.0,))
+
 
 def write_header(fid, version, date, time):
     fid.write('PALMRobo Elements\n')
     fid.write('Version:,{}\n'.format(version))
-    fid.write('Date, Time:,{},{}\n'.format(date,time))
+    fid.write('Date, Time:,{},{}\n'.format(date, time))
     fid.write('\n')
     fid.write('MICROMETER\n')
     fid.write('Elements :\n')
     fid.write('\n')
-    
-version='V 4.8.0.1'
-date='26/11/18'
-time='17:20:54'
-column_names = ["Type", "Color", "Thickness", "No", "CutShot", "Area", "Z", "Comment", "Coordinates"]
+
+
+version = 'V 4.8.0.1'
+date = '26/11/18'
+time = '17:20:54'
+column_names = ["Type", "Color", "Thickness", "No",
+                "CutShot", "Area", "Z", "Comment", "Coordinates"]
 
 try:
     file_to_convert = sys.argv[1]
@@ -26,17 +38,17 @@ except IndexError:
 
 # Open input IA file and output csv file
 try:
-    IA_file = open(file_to_convert, 'r');
+    IA_file = open(file_to_convert, 'r')
 except:
     sys.exit('Error: File {} not found'.format(file_to_convert))
-    
+
 # Open output CSV file
 rootname, ext = os.path.splitext(file_to_convert)
 converted_file = '{}_converted_to_LCM.csv'.format(rootname)
 if os.path.isfile(converted_file):
     os.remove(converted_file)
 csv_file = open(converted_file, 'a')
-    
+
 write_header(csv_file, version, date, time)
 
 for s in column_names[:-1]:
@@ -47,25 +59,26 @@ csv_file.write('\n\n')
 df_head = pd.read_csv('values.csv', sep='\t', names=column_names)
 
 
-
 for i, line in enumerate(IA_file):
     # Write first two rows
-    df_head[i:i+1].to_csv(csv_file, sep=',',index=False, header=False)
+    df_head[i:i+1].to_csv(csv_file, sep=',', index=False, header=False)
 
-    #Parse IA file and build list of tuples (x,y)
+    # Parse IA file and build list of tuples (x,y)
     line = line.split(":")
 
     filter_start = [s.startswith(' ') for s in line]
-    filtered_list = [i.replace(', Point','').strip() for (i, v) in zip(line, filter_start) if v]
-    filtered_list[-1] = filtered_list[-1].replace(']','')
+    filtered_list = [i.replace(', Point', '').strip()
+                     for (i, v) in zip(line, filter_start) if v]
+    filtered_list[-1] = filtered_list[-1].replace(']', '')
 
-    list_of_tuples = [(float(s.split(', ')[0]),float(s.split(', ')[1])) for s in filtered_list]
+    list_of_tuples = [(float(s.split(', ')[0]), float(s.split(', ')[1]))
+                      for s in filtered_list]
 
     do_transformation(list_of_tuples)
 
     # Convert list of tuples to list of string
     # ["x1,y1", "x2,y2"...]
-    list_of_strings = ['{},{}'.format(t[0],t[1]) for t in list_of_tuples]
+    list_of_strings = ['{},{}'.format(t[0], t[1]) for t in list_of_tuples]
 
     # Reshape list of strings into a list of lists.
     # Each sublist has 5 elements
@@ -78,21 +91,19 @@ for i, line in enumerate(IA_file):
     iterator = iter(list_of_strings)
     alist = [[next(iterator) for _ in range(5)] for sublist in range(nb_sublists)]
 
-    if N%5:
-        rest_list = list_of_strings[-(N%5):]
+    if N % 5:
+        rest_list = list_of_strings[-(N % 5):]
         rest_list = rest_list + ['']*(5-len(rest_list))
         alist.append(rest_list)
-        
+
     # Add dot in position 0 for each sublist
     for sublist in alist:
-        sublist.insert(0,".")
-        
+        sublist.insert(0, ".")
+
     # Build pandas DataFrame from list of lists
     # and update output csv file
     df = pd.DataFrame(alist)
-    df.to_csv(csv_file, sep=',',index=False, header=False)
+    df.to_csv(csv_file, sep=',', index=False, header=False)
 
     # Jump to lines and loop back to next line in IA input file
     csv_file.write('\n\n')
-
-
